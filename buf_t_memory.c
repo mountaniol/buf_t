345b213747554888ef58de182ec956835fc4dd5c
Sebastian Mountaniol <mountaniol@gmail.com>
2020-06-18 20:53:37 +0200

    Initial commit

---
+++ buf_t_memory.c
@@ -0,0 +1,29 @@
+/*@-skipposixheaders@*/
+#include <stdlib.h>
+#include <string.h>
+/*@=skipposixheaders@*/
+
+/*@null@*/ /*@only@*/void *zmalloc(size_t sz)
+{
+	/*@shared@*/void *ret = malloc(sz);
+	if (NULL == ret) return (NULL);
+	memset(ret, 0, sz);
+	return (ret);
+}
+
+
+/*@null@*/ /*@only@*/void *zmalloc_any(size_t asked, size_t *allocated)
+{
+	/*@shared@*/void *ret = NULL;
+
+	while (NULL == ret && asked > 0) {
+		ret = zmalloc(asked);
+		if (NULL != ret) {
+			*allocated = asked;
+			return (ret);
+		}
+		asked /= 2;
+	}
+	return NULL;
+}
+
